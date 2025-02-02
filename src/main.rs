afe0269f284c0cbb9af3df4ed28e55a08ed6d144
Fifixex <122852085+Fifixex@users.noreply.github.com>
2025-02-02 07:47:00 +0000

    🥅 Refactor: error handling with `Result<T, E>` and rename `new` to `build`

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 use std::env;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::build(&args)?;
     println!("Host: {}\nPort: {}", config.host, config.port);
+    Ok(())
 }
 
 // I know this is improvable but I'm following the rust book I/O project
 // plz wait without complaining until I finish the book
 struct Config {
     host: String,
-    port: String
+    port: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let host = args[1].clone();
-    let port = args[2].clone();
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-    Config {
-        host,
-        port
+        let host = args[1].clone();
+        let port = args[2].clone();
+
+        Ok(Config { host, port })
     }
 }
